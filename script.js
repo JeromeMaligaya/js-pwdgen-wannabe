559f18f540410693e0a117bf7feeb46c2091836a
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-07-30 17:56:12 +0200

    raccolta dati

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ console.log('JS OK')
 // 1.recupero l'elemento dal Dom
 const resultelement = document.getElementById('result')
 console.log(resultelement)
+
+//2.raccolta dati
+    // chiedo all'utente il suo nome
+    const username = prompt('Digita il tuo nome', 'Jerome')
+    console.log(username)
+    // chiedo all'utente il suo cognome
+    const usersurname = prompt('Digita il tuo cognome', 'Maligaya')
+    console.log(usersurname)
+    // chiedo all'utente il suo nome
+    const usercolor = prompt('Digita il tuo colore', 'White')
+    console.log(usercolor)
