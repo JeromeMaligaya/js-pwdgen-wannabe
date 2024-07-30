e6716fa9bced714b90259a6cfc52fe4ac58a1510
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-07-30 18:24:17 +0200

    lavorazione dati

--- script.js
+++ script.js
@@ -2,16 +2,32 @@
 console.log('JS OK')
 
 // 1.recupero l'elemento dal Dom
-const resultelement = document.getElementById('result')
-console.log(resultelement)
+const resultElement = document.getElementById('result')
+console.log(resultElement)
 
 //2.raccolta dati
     // chiedo all'utente il suo nome
-    const username = prompt('Digita il tuo nome', 'Jerome')
-    console.log(username)
+    const userName = prompt('Digita il tuo nome', 'Jerome');
+    console.log(userName)
     // chiedo all'utente il suo cognome
-    const usersurname = prompt('Digita il tuo cognome', 'Maligaya')
-    console.log(usersurname)
+    const userSurname = prompt('Digita il tuo cognome', 'Maligaya');
+    console.log(userSurname)
     // chiedo all'utente il suo nome
-    const usercolor = prompt('Digita il tuo colore', 'White')
-    console.log(usercolor)
+    const userColor = prompt('Digita il tuo colore', 'White');
+    console.log(userColor)
+// fine
+
+// 3.lavorazione dati
+    //concatenazione stringhe
+    const userInfo = userName + userSurname + userColor;
+    console.log(userInfo)
+    //generazione password
+    let pwNumber = 97;
+    const pwUser = userInfo + pwNumber;
+    console.log(pwUser)
+    //interpolazione messaggio
+    const message = `la tua nuova password: ${pwUser}`
+// fine
+
+
+
